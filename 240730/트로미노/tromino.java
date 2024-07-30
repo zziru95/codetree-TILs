20481635ec4b6ec4b65c545497e3a0a8558e16c7
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-07-30 20:07:11 +0900

    Title: 트로미노, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240730/트로미노/tromino.java
@@ -0,0 +1,104 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    //하,우,상,좌
+    static int[][] direction= {{1,0},  {0,1}, {-1,0}, {0,-1}};
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] graph = new int[n][m];
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<m;j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int answer = Math.max(check1(graph,n,m), check2(graph,n,m));
+        System.out.println(answer);
+
+
+    }
+
+    public static int check1(int[][] graph, int n, int m) {
+        int maxValue = 0;
+        for(int i=0; i<n-2; i++) {
+            for(int j=0; j<m-2; j++) {
+                int temp1 = 0;
+                int temp2 = 0 ;
+                for(int k=0; k<3; k++) {
+                    temp1 += graph[i][j+k];
+                    temp2 += graph[i+k][j];
+                }
+                maxValue = Math.max(maxValue, Math.max(temp1,temp2));
+            }
+        }
+        return maxValue;
+    }
+
+    public static int check2(int[][] graph, int n, int m) {
+        Queue<Node> q = new LinkedList<>();
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<m-1; j++) {
+                q.add(new Node(i,j));
+            }
+        }
+
+        int maxValue =0;
+
+        while(!q.isEmpty()) {
+            Node now = q.poll();
+            int r = now.row;
+            int c = now.col;
+
+            for(int d=0; d<4; d++) {
+                int dr = r + direction[d][0];
+                int dc = c + direction[d][1];
+                if(0<=dr && dr<n && 0<= dc && dc<m) {
+                    for(int e=0; e<4; e++) {
+                        if(d%2 == e%2) continue;
+
+                        int temp = graph[r][c] + graph[dr][dc];
+                        
+                        int er = dr + direction[e][0];
+                        int ec = dc + direction[e][1];
+
+                        if(0<=er && er<n && 0<= ec && ec<m) {
+                            maxValue = Math.max(maxValue, (temp+graph[er][ec]));
+                        }
+
+
+
+                        int fr = r + direction[e][0];
+                        int fc = c + direction[e][1];
+
+                        if(0<=fr && fr<n && 0<= fc && fc<m) {
+                            maxValue = Math.max(maxValue, (temp+graph[fr][fc]));
+                        }
+
+                }
+
+                }
+
+
+            }
+        }
+
+
+        return maxValue;
+    }
+
+    static class Node{
+        int row;
+        int col;
+        public Node(int row, int col)  {
+            this.row = row;
+            this.col = col;
+        }
+    }
+}
