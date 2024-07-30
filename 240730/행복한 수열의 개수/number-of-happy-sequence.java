83cb8e7881505c95dd8d0fbf711f1137bb18e5e3
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-07-30 18:02:03 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240730/행복한 수열의 개수/number-of-happy-sequence.java
@@ -10,7 +10,6 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int[][] graph = new int[n][n];
         int answer = 0;
-        int temp;
 
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -21,17 +20,29 @@ public class Main {
 
 
         for(int i=0; i<=n-m; i++) {
-            for(int j=0; j<=-n-m; j++) {
-                temp = graph[i][j];
-                boolean check1 = true;
-                boolean check2 = true;
-                for(int k=1; k<=m; k++) {
-                    if (graph[i+k][j] != temp) check1 = false;
-                    if (graph[i][j+k] != temp) check2 = false;
-                }
-                if(check1) answer++;
-                if(check2) answer++;
+            boolean check1 = false;
+            boolean check2 = false;
+            int j=0;
+            int temp = 1;
+            int temp2 = 1;
+            while(j<n-1) {
+                if(graph[i][j] == graph[i][j+1]) {
+                    temp++;
+                    } else {
+                        temp = 1;
+                        }
+                if(temp >= m) check1 = true;
+
+                if(graph[j][i] == graph[j+1][i+1]) {temp2++;}
+                else {temp2=1;}
+                if(temp2 >= m ) check2 = true;
+
+                j++;
+        
             }
+
+            if(check1) answer++; // 세로 방향이 일치하면 결과 증가
+            if(check2) answer++; // 가로 방향이 일치하면 결과 증가
         }
         System.out.println(answer);
     }
