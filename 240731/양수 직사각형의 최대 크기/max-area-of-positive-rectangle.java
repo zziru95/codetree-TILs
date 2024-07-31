52e10c48a1ef0580420b60def46a06a59ee36f23
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-07-31 15:37:40 +0900

    Title: 양수 직사각형의 최대 크기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240731/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int maxCnt = 0;
+        for (int r1 = 0; r1 < n ; r1++) {
+            for (int c1 = 0; c1 < m; c1++) {
+                for (int r2 = r1; r2 < n; r2++) {
+                    for (int c2 = c1; c2 < m; c2++) {
+                        if (matrix[r2][c2] <= 0) {
+                            break;
+                        }
+                        maxCnt = Math.max(maxCnt, (r2 - r1 + 1) * (c2 - c1 + 1));
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxCnt);
+    }
+
+}
