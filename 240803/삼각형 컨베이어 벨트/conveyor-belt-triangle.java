4c2b3e0f598cb8c9831b018563b4bd05c1cf1ce7
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-08-03 16:08:07 +0900

    Title: 삼각형 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240803/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int[][] triangle = new int[3][n];
+
+        for(int i=0; i<3; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int t=0; t<T; t++) {
+            int temp1 = temp[2][n-1];
+            int temp2 = temp[0][n-1];
+            int temp3 = temp[1][n-1];
+
+            for(int i=1; i<n-1; i++) {
+                temp[0][n-i] = temp[0][n-i-1];
+                temp[1][n-i] = temp[1][n-i-1];
+                temp[2][n-i] = temp[2][n-i-1];
+            }
+            temp[0][0] = temp1;
+            temp[1][0] = temp2;
+            temp[2][0] = temp3;
+        }
+
+        for(int i=0; i<3; i++) {
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<n; j++) {
+                sb.append(triangle[i][j]);
+
+                if(j<n-1) sb.append(" ");
+            }
+            System.out.println(sb.toString());
+        }
+
+    }
+}
