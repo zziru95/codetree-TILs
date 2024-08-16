e5177d79e5a1c6844e46c9c73a709b5ca1db709d
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-08-16 15:25:20 +0900

    Title: 십자 모양의 지속적 폭발, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 240816/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.java
+++ 240816/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.java
@@ -14,8 +14,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        graph = new int[n][n];
 
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -26,11 +27,61 @@ public class Main {
 
         for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
-            bomb(Integer.parseInt(st.nextToken()));
+            bomb(Integer.parseInt(st.nextToken())-1);
+            gravity();
+        }
+        printAnswer();
+
+
+    }
+
+    public static void bomb(int c) {
+
+        for(int i=0; i<n; i++) {
+            if(graph[i][c] != 0) {
+                int cnt = graph[i][c];
+
+                for(int d=0; d<4; d++) {
+                    for(int k=0; k<cnt; k++) {
+                        int nr = i + direction[d][0] * k;
+                        int nc = c + direction[d][1] * k;
+                        if(0<=nr && nr<n && 0<=nc && nc<n) {
+                            graph[nr][nc] = 0;
+                        }
+                    }
+                }
+                return;
+            }
         }
 
 
     }
 
-    public static void
+    public static void gravity() {
+        int[][] temp = new int[n][n];
+        for(int j=0; j<n; j++) {
+            int idx = n-1;
+            for(int i=n-1; i>=0; i--) {
+                if(graph[i][j] !=0) {
+                    temp[idx][j] = graph[i][j];
+                    idx--;
+                }
+            }
+        }
+
+        graph = temp;
+    }
+
+    public static void printAnswer() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                sb.append(graph[i][j]);
+                if(j<n-1) sb.append(" ");
+            }
+            if(i < n - 1) sb.append(System.lineSeparator());
+        }
+
+        System.out.print(sb);
+    }
 }
