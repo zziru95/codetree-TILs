4368d219d07ddd1c7cae923948bad78bb9643ff2
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-08-31 17:26:10 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 3043ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240831/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int r,c;
+    static char[][] miro;
+    static int[][] direction = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}}; // 우,상,좌,하
+
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken())-1;
+        c = Integer.parseInt(st.nextToken())-1;
+        miro = new char[n][n];
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine().trim();
+            for (int j = 0; j < n; j++) {
+                miro[i][j] = line.charAt(j);
+            }
+        }
+        visited[r][c] = true;
+        System.out.println(goMiro(r,c));
+
+    }
+
+    public static int goMiro(int r, int c) {
+        int d = 0;
+        int cnt = 0;
+        while (true) {
+            //다음곳
+            int nr = r + direction[d][0];
+            int nc = c + direction[d][1];
+            //벽위치
+            int wr = r + direction[(d+3)%4][0];
+            int wc = c + direction[(d+3)%4][1];
+
+            if (checkIndex(nr, nc) && miro[nr][nc] == '#') {
+                d = (d + 1) % 4;
+                continue;
+            }
+
+            if (checkIndex(wr,wc) && miro[wr][wc] == '#' && !checkIndex(nr,nc)) return cnt+1;
+
+            if (checkIndex(nr, nc) && miro[nr][nc] == '.' && checkIndex(wr, wc) && miro[wr][wc] == '#') {
+                cnt++;
+                r = nr;
+                c = nc;
+                wr = r + direction[(d+3)%4][0];
+                wc = c + direction[(d+3)%4][1];
+                if (checkIndex(wr, wc) && miro[wr][wc] == '.') {
+                    cnt++;
+                    d = (d + 3) % 4;
+                    r = r + direction[d][0];
+                    c = c + direction[d][1];
+                }
+
+                continue;
+            }
+
+
+
+
+            break;
+        }
+
+        return -1;
+    }
+
+    public static boolean checkIndex(int r, int c) {
+        return 0 <= r && r < n && 0 <= c && c < n;
+    }
+}
