9ef79cc2ae6bb3de060dc0615ea55a80b4538d90
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-09-24 13:53:08 +0900

    Title: 핀볼게임, Time: 3083ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 240924/핀볼게임/pinball-game.java
+++ 240924/핀볼게임/pinball-game.java
@@ -4,19 +4,12 @@ import java.io.*;
 
 public class Main {
     static int[][] direction = { {-1,0}, {1,0}, {0,1}, {0,-1} }; // 상하우좌
-    static Map<Integer, Integer> bar1;
-    static Map<Integer, Integer> bar2;
+    static int[] bar2 = {3,2,1,0};
     static int[][] frame;
     static int n;
     static int maxValue;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
-        bar1 = new HashMap<>();
-        bar2 = new HashMap<>();
-        bar2.put(0,3);
-        bar2.put(1,2);
-        bar2.put(2,1);
-        bar2.put(3,0);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -54,7 +47,7 @@ public class Main {
             if(frame[r][c] == 1) {
                 d = (d+2)%4;
             } else if(frame[r][c] == 2) {
-                d = bar2.get(d);
+                d = bar2[d];
             }
         }
 
