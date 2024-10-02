49e651f74641f2a68d306546f6c573f8d04da1b8
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-02 18:05:02 +0900

    Title: 합쳐지는 구슬들, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241002/합쳐지는 구슬들/merge-marbles.java
+++ 241002/합쳐지는 구슬들/merge-marbles.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
 
-    static class Bid{
-        int r,c,d,w,num;
+    static class Bid {
+        int r, c, d, w, num;
 
         public Bid(int r, int c, int d, int w, int num) {
             this.r = r;
@@ -13,19 +14,31 @@ public class Main {
             this.w = w;
             this.num = num;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Bid bid = (Bid) o;
+            return r == bid.r && c == bid.c;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(r, c);
+        }
     }
 
-    static int n,m,t;
+    static int n, m, t;
     static Bid[][] arr;
     static Bid[][] nextArr;
-    static Set<int[]> bids;
+    static Set<Bid> bids;
 
     static int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    static int[] change = {1,0,3,2};
+    static int[] change = {1, 0, 3, 2};
 
     static int maxW;
-
 
     public static void main(String[] args) throws IOException {
 
@@ -41,17 +54,18 @@ public class Main {
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken())-1;
-            int c = Integer.parseInt(st.nextToken())-1;
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
             char D = st.nextToken().charAt(0);
             int w = Integer.parseInt(st.nextToken());
             int d = -1;
-            if(D=='U') d=0;
-            if(D=='D') d=1;
-            if(D=='L') d=2;
-            if(D=='R') d=3;
-            bids.add(new int[]{r, c});
-            arr[r][c] =new Bid(r, c, d, w, i);
+            if (D == 'U') d = 0;
+            if (D == 'D') d = 1;
+            if (D == 'L') d = 2;
+            if (D == 'R') d = 3;
+            Bid newBid = new Bid(r, c, d, w, i);
+            bids.add(newBid);
+            arr[r][c] = newBid;
         }
 
         for (int i = 0; i < t; i++) {
@@ -62,28 +76,26 @@ public class Main {
         sb.append(" ");
         sb.append(maxW);
         System.out.print(sb);
-
     }
 
     public static int move() {
-        Set<int[]> nextBids = new HashSet<>();
+        Set<Bid> nextBids = new HashSet<>();
         nextArr = new Bid[n][n];
         int cnt = 0;
-        for (int[] now : bids) {
-            Bid nowBids = arr[now[0]][now[1]];
-            int d = nowBids.d;
-            int w = nowBids.w;
-            int num = nowBids.num;
-            int nr = nowBids.r + direction[d][0];
-            int nc = nowBids.c + direction[d][1];
+        for (Bid nowBid : bids) {
+            int d = nowBid.d;
+            int w = nowBid.w;
+            int num = nowBid.num;
+            int nr = nowBid.r + direction[d][0];
+            int nc = nowBid.c + direction[d][1];
             if (!(0 <= nr && nr < n && 0 <= nc && nc < n)) {
-                nr = nowBids.r;
-                nc = nowBids.c;
+                nr = nowBid.r;
+                nc = nowBid.c;
                 d = change[d];
             }
             if (nextArr[nr][nc] == null) {
-                nextArr[nr][nc] = nowBids;
-                nextBids.add(new int[]{nr, nc});
+                nextArr[nr][nc] = new Bid(nr, nc, d, w, num);
+                nextBids.add(nextArr[nr][nc]);
                 cnt++;
             } else {
                 Bid another = nextArr[nr][nc];
