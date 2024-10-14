5175880872fdb79a136066d57202975adfb6a80d
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-14 14:24:03 +0900

    Title: n개의 점 중 m개 고르기, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241014/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n,m;
+    static int[][] dots;
+    static int[][] comb;
+    static int minD = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        dots = new int[n][2];
+        comb = new int[m][2];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            dots[i][0] = Integer.parseInt(st.nextToken());
+            dots[i][1] = Integer.parseInt(st.nextToken());
+        }
+        choose(0,0);
+        System.out.print(minD);
+
+    }
+    public static void choose(int start, int cnt){
+        if(cnt ==m){
+            calcD();
+            return;
+        }
+
+        for(int i=start; i<n; i++){
+            comb[cnt]= dots[i];
+            choose(i+1, cnt+1);
+        }
+
+    }
+
+    public static void calcD(){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<m; j++){
+                if(i==j) continue;
+                int newD = (comb[i][0]-comb[j][0]) * (comb[i][0]-comb[j][0]) + (comb[i][1]-comb[j][1])* (comb[i][1]-comb[j][1]);
+                minD = Math.min(minD,newD); 
+            }
+        }
+    }
+}
