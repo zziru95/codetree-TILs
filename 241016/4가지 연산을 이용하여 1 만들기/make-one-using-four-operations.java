f08a6384742d802297122ba19778d941963a5d84
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-16 03:43:40 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241016/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 241016/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -3,60 +3,31 @@ import java.io.*;
 
 public class Main {
     static int N;
+    
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        // System.out.print(bfs());
-        int[] memo = new int[N+1];
+        N = Integer.parseInt(br.readLine());
+
+        int[] memo = new int[N + 1];
         
-        for(int i=2;i<=N;i++){
-            memo[i] = memo[i-1] +1;
-            if(i%2==0){
-                memo[i] = Math.min(memo[i], memo[i/2]+1);
+        // 초기값 설정
+        memo[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            // 1을 뺀 경우
+            memo[i] = memo[i - 1] + 1;
+
+            // 2로 나누어 떨어지는 경우
+            if (i % 2 == 0) {
+                memo[i] = Math.min(memo[i], memo[i / 2] + 1);
             }
-            if(i%3==0){
-                memo[i] = Math.min(memo[i], memo[i/3]+1);
+
+            // 3으로 나누어 떨어지는 경우
+            if (i % 3 == 0) {
+                memo[i] = Math.min(memo[i], memo[i / 3] + 1);
             }
         }
 
         System.out.print(memo[N]);
-
     }
-
-    // public static int bfs(){
-    //     Queue<int[]> q = new ArrayDeque<>();
-    //     Set<Integer> visited = new HashSet<>();
-    //     q.add(new int[]{N,0});
-    //     visited.add(N);
-    //     while(!q.isEmpty()){
-    //         int[] current = q.poll();
-    //         int now = current[0];
-    //         int t = current[1];
-    //         if(now==1) return t;
-    //         for(int temp : search(now)){
-    //             if(!visited.contains(temp)){
-    //                 q.add(new int[]{temp,t+1});
-    //                 visited.add(temp);
-    //             }
-    //         }
-    //     }
-
-
-    //     return -1;
-    // }
-
-    // public static ArrayList<Integer> search(int n){
-    //     ArrayList<Integer> temp = new ArrayList<>();
-    //     temp.add(n-1);
-    //     temp.add(n+1);
-    //     if(n!=0 &&n%2 ==0){
-    //         temp.add(n/2);
-    //     }
-    //     if(n!=0 &&n%3 ==0){
-    //         temp.add(n/3);
-    //     }
-    //     return temp;
-    // }
 }
