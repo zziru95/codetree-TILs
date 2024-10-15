b5ae5479d9d4e271d2f41227dbf938dbeb7be1d4
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-16 03:31:59 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 140ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241016/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        System.out.print(bfs());
+
+    }
+
+    public static int bfs(){
+        Queue<int[]> q = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        q.add(new int[]{N,0});
+        visited.add(N);
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            int now = current[0];
+            int t = current[1];
+            if(now==1) return t;
+            for(int temp : search(now)){
+                if(!visited.contains(temp)){
+                    q.add(new int[]{temp,t+1});
+                    visited.add(temp);
+                }
+            }
+        }
+
+
+        return -1;
+    }
+
+    public static ArrayList<Integer> search(int n){
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(n-1);
+        temp.add(n+1);
+        if(n!=0 &&n%2 ==0){
+            temp.add(n/2);
+        }
+        if(n!=0 &&n%3 ==0){
+            temp.add(n/3);
+        }
+        return temp;
+    }
+}
