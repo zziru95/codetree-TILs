005884fd0f2bc2cccb934e07e856cad740e9565e
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-16 18:38:30 +0900

    Title: 가장 먼저 나오는 숫자, Time: 389ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 241016/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n,m;
+    static int[] nums;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        
+        StringBuilder sb =new StringBuilder();
+
+        for(int i=0; i<m; i++){
+            sb.append(binSearch(Integer.parseInt(st.nextToken())));
+            if(i<m-1){
+                sb.append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+
+
+
+    public static int binSearch(int num){
+        int left = 0;
+        int right = n-1;
+        int minIdx = n;
+        boolean check = false;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]>=num){
+                minIdx=Math.min(minIdx,mid);
+                right = mid-1;
+                if(nums[mid]==num) check = true;
+            } else{
+                left = mid+1;
+            }
+        }
+
+        if(check) return minIdx+1;
+        return -1;
+    }
+}
