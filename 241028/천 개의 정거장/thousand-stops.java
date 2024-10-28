9c8aa6671854b9a42f1cd3571bec40ffc355f698
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-28 23:04:35 +0900

    Title: 천 개의 정거장, Time: 2068ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241028/천 개의 정거장/thousand-stops.java
+++ 241028/천 개의 정거장/thousand-stops.java
@@ -2,33 +2,21 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Pair {
+    static class Edge {
         int to;
-        long w;
+        long fare;
         int time;
 
-        public Pair(int to, long w, int time) {
+        public Edge(int to, long fare, int time) {
             this.to = to;
-            this.w = w;
-            this.time = time;
-        }
-    }
-
-    static class Node {
-        int to;
-        long w;
-        long time;
-
-        public Node(int to, long w, long time) {
-            this.to = to;
-            this.w = w;
+            this.fare = fare;
             this.time = time;
         }
     }
 
     static int A, B, N;
-    static List<Pair>[] graph;
-    static long INF = Long.MAX_VALUE;
+    static final long INF = Long.MAX_VALUE;
+    static List<Edge>[] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,70 +26,79 @@ public class Main {
         B = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
+        // 인접 리스트 초기화
         graph = new ArrayList[1001];
-        for (int i = 0; i < 1001; i++) {
+        for (int i = 0; i <= 1000; i++) {
             graph[i] = new ArrayList<>();
         }
 
+        // 각 버스의 노선 입력 처리
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            long price = Long.parseLong(st.nextToken());
-            int stopCnt = Integer.parseInt(st.nextToken());
+            long fare = Long.parseLong(st.nextToken());
+            int stopCount = Integer.parseInt(st.nextToken());
 
-            int[] route = new int[stopCnt];
+            int[] route = new int[stopCount];
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < stopCnt; j++) {
+            for (int j = 0; j < stopCount; j++) {
                 route[j] = Integer.parseInt(st.nextToken());
             }
 
-            for (int j = 0; j < stopCnt - 1; j++) {
-                for (int k = j + 1; k < stopCnt; k++) {
-                    int time = k - j;
-                    graph[route[j]].add(new Pair(route[k], price, time));
+            // 노선 연결 정보 추가 (인접 리스트에 저장)
+            for (int j = 0; j < stopCount - 1; j++) {
+                for (int k = j + 1; k < stopCount; k++) {
+                    int from = route[j];
+                    int to = route[k];
+                    int travelTime = k - j;
+
+                    graph[from].add(new Edge(to, fare, travelTime));
                 }
             }
         }
 
-        long[] answer = dijkstra(A, B);
-        System.out.println(answer[0] + " " + answer[1]);
+        // 다익스트라 알고리즘 실행
+        long[] result = dijkstra(A, B);
+        System.out.println(result[0] + " " + result[1]);
     }
 
+    // 다익스트라 알고리즘 구현
     public static long[] dijkstra(int start, int end) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1.w == o2.w) {
-                return Long.compare(o1.time, o2.time);
-            }
-            return Long.compare(o1.w, o2.w);
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> {
+            if (a.fare == b.fare) return Integer.compare(a.time, b.time);
+            return Long.compare(a.fare, b.fare);
         });
 
-        long[] minCost = new long[1001];
+        long[] minFare = new long[1001];
         long[] minTime = new long[1001];
-        Arrays.fill(minCost, INF);
+        Arrays.fill(minFare, INF);
         Arrays.fill(minTime, INF);
 
-        minCost[start] = 0;
+        minFare[start] = 0;
         minTime[start] = 0;
-        pq.add(new Node(start, 0, 0));
+        pq.add(new Edge(start, 0, 0));
 
         while (!pq.isEmpty()) {
-            Node curr = pq.poll();
+            Edge curr = pq.poll();
 
             if (curr.to == end) {
-                return new long[]{curr.w, curr.time};
+                return new long[]{minFare[end], minTime[end]};
             }
 
-            for (Pair next : graph[curr.to]) {
-                long newCost = curr.w + next.w;
-                long newTime = curr.time + next.time;
+            // 현재 노드에서 연결된 간선 탐색
+            for (Edge next : graph[curr.to]) {
+                long newFare = minFare[curr.to] + next.fare;
+                int newTime = (int) (minTime[curr.to] + next.time);
 
-                if (newCost < minCost[next.to] || (newCost == minCost[next.to] && newTime < minTime[next.to])) {
-                    minCost[next.to] = newCost;
+                if (newFare < minFare[next.to] || 
+                    (newFare == minFare[next.to] && newTime < minTime[next.to])) {
+                    minFare[next.to] = newFare;
                     minTime[next.to] = newTime;
-                    pq.add(new Node(next.to, newCost, newTime));
+                    pq.add(new Edge(next.to, newFare, newTime));
                 }
             }
         }
 
+        // 경로가 없을 때
         return new long[]{-1, -1};
     }
 }
