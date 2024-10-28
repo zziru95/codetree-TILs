df6cbe2ebcc0a293ccd5ddbf1bcf44dd8c1a1dbe
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-28 22:45:33 +0900

    Title: 천 개의 정거장, Time: 2204ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241028/천 개의 정거장/thousand-stops.java
+++ 241028/천 개의 정거장/thousand-stops.java
@@ -7,7 +7,7 @@ public class Main {
         long w;
         int bus;
         int time;
-        public Pair(int to, long w, int bus, int time){
+        public Pair(int to, long w, int bus, int time) {
             this.to = to;
             this.w = w;
             this.bus = bus;
@@ -15,12 +15,12 @@ public class Main {
         }
     }
 
-    static class Node{
+    static class Node {
         int to;
         long w;
         int bus;
         long time;
-        public Node(int to, long w, int bus, long time){
+        public Node(int to, long w, int bus, long time) {
             this.to = to;
             this.w = w;
             this.bus = bus;
@@ -28,101 +28,107 @@ public class Main {
         }
     }
 
-    static class Cost{
+    static class Cost {
         long w;
         long time;
-        public Cost(long w, long time){
+        public Cost(long w, long time) {
             this.w = w;
             this.time = time;
         }
     }
 
-    static int A,B,N;
-    static ArrayList<Pair>[] graph;
-    static long INF = (long)1e11;
+    static int A, B, N;
+    static ArrayList<HashMap<Integer, Pair>>[] graph;
+    static long INF = (long) 1e11;
 
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        graph = new ArrayList[1001];
-        for(int i=0; i<1001; i++){
-            graph[i] = new ArrayList<>();
+        graph = new ArrayList[1001];  // 각 정류장에 대한 ArrayList 초기화
+        for (int i = 0; i < 1001; i++) {
+            graph[i] = new ArrayList<>();  // 각 정류장에 ArrayList 생성
         }
+
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
-        N= Integer.parseInt(st.nextToken());
-        
-        for(int i=1; i<=N;i++){
+        N = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             long price = Long.parseLong(st.nextToken());
             int stopCnt = Integer.parseInt(st.nextToken());
             int[] route = new int[stopCnt];
+
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<stopCnt;j++) {
+            for (int j = 0; j < stopCnt; j++) {
                 route[j] = Integer.parseInt(st.nextToken());
             }
-            for(int j = 0; j < stopCnt - 1; j++){
+
+            for (int j = 0; j < stopCnt - 1; j++) {
                 long cumulativeTime = 0;
-                for(int k = j + 1; k < stopCnt; k++){
-                    cumulativeTime += 1; // 각 정류장 간 시간 (1로 가정)
-                    if(check(route[j],route[k],price,cumulativeTime)){
-                        graph[route[j]].add(new Pair(route[k], price, i, (int)cumulativeTime));
+                for (int k = j + 1; k < stopCnt; k++) {
+                    cumulativeTime += 1;  // 각 정류장 간 시간 (1로 가정)
+                    if (check(route[j], route[k], price, cumulativeTime)) {
+                        HashMap<Integer, Pair> map = new HashMap<>();
+                        map.put(route[k], new Pair(route[k], price, i, (int) cumulativeTime));
+                        graph[route[j]].add(map);  // 그래프에 경로 추가
                     }
                 }
             }
-
         }
 
-        long[] answer = dijk(A,B);
-        System.out.print(answer[0]+" "+answer[1]);
+        long[] answer = dijk(A, B);
+        System.out.print(answer[0] + " " + answer[1]);
     }
 
-
-    public static long[] dijk(int A, int B){
-        long[] answer = new long[] {-1,-1};
+    public static long[] dijk(int A, int B) {
+        long[] answer = new long[] { -1, -1 };
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-        int cmp = Long.compare(o1.w, o2.w);
-        if (cmp == 0) {
-            return Long.compare(o1.time, o2.time);
-        }
-        return cmp;
+            int cmp = Long.compare(o1.w, o2.w);
+            if (cmp == 0) {
+                return Long.compare(o1.time, o2.time);
+            }
+            return cmp;
         });
+
         Cost[] dist = new Cost[1001];
         for (int i = 0; i < dist.length; i++) {
             dist[i] = new Cost(INF, INF);
         }
-        dist[A] = new Cost(0,0);
-        pq.add(new Node(A,0,0,0));
+        dist[A] = new Cost(0, 0);
+        pq.add(new Node(A, 0, 0, 0));
 
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             Node curr = pq.poll();
             int now = curr.to;
             long w = curr.w;
             int bus = curr.bus;
             long time = curr.time;
 
-            if(compare(new Cost(w,time),dist[now])) continue;
-            if(now == B) {
+            if (compare(new Cost(w, time), dist[now])) continue;
+            if (now == B) {
                 answer[0] = w;
                 answer[1] = time;
                 break;
             }
-            for(Pair temp :graph[now]){
-                int next = temp.to;
-                int nextBus = temp.bus;
-                long newW = w;
-                long newTime = time + temp.time;
-                newW+=temp.w;
-                
-                Cost newCost = new Cost(newW,newTime);
-                if(compare(dist[next],newCost)){
-                    dist[next] = newCost;
-                    pq.add(new Node(next,newW,nextBus,newTime));
+
+            for (HashMap<Integer, Pair> map : graph[now]) {
+                for (Map.Entry<Integer, Pair> entry : map.entrySet()) {
+                    int next = entry.getKey();
+                    Pair temp = entry.getValue();
+                    int nextBus = temp.bus;
+                    long newW = w;
+                    long newTime = time + temp.time;
+                    newW += temp.w;
+
+                    Cost newCost = new Cost(newW, newTime);
+                    if (compare(dist[next], newCost)) {
+                        dist[next] = newCost;
+                        pq.add(new Node(next, newW, nextBus, newTime));
+                    }
                 }
             }
         }
-
 
         return answer;
     }
@@ -133,13 +139,14 @@ public class Main {
 
     public static boolean check(int start, int to, long price, long time) {
         for (int i = graph[start].size() - 1; i >= 0; i--) {  // 뒤에서부터 순회
-            Pair temp = graph[start].get(i);
-            if (temp.to != to) continue;  // 목적지가 다르면 넘어감
-            if (temp.w < price || (temp.w == price && temp.time < time)) {
-                // 조건 만족 시 삭제
-                return false;
+            HashMap<Integer, Pair> map = graph[start].get(i);
+            if (map.containsKey(to)) {
+                Pair temp = map.get(to);
+                if (temp.w < price || (temp.w == price && temp.time < time)) {
+                    return false;  // 더 빠르거나 저렴한 경로가 있으면 추가하지 않음
+                }
+                graph[start].remove(i);  // 기존 경로 삭제
             }
-            graph[start].remove(i);
         }
         return true;
     }
