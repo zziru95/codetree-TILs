8cd86b1b75bebc0cdad024d233a477b9bd5dbf9e
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-28 23:01:57 +0900

    Title: 천 개의 정거장, Time: 2459ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241028/천 개의 정거장/thousand-stops.java
+++ 241028/천 개의 정거장/thousand-stops.java
@@ -5,13 +5,11 @@ public class Main {
     static class Pair {
         int to;
         long w;
-        int bus;
         int time;
 
-        public Pair(int to, long w, int bus, int time) {
+        public Pair(int to, long w, int time) {
             this.to = to;
             this.w = w;
-            this.bus = bus;
             this.time = time;
         }
     }
@@ -19,123 +17,91 @@ public class Main {
     static class Node {
         int to;
         long w;
-        int bus;
         long time;
 
-        public Node(int to, long w, int bus, long time) {
+        public Node(int to, long w, long time) {
             this.to = to;
-            this.w = w;
-            this.bus = bus;
-            this.time = time;
-        }
-    }
-
-    static class Cost {
-        long w;
-        long time;
-
-        public Cost(long w, long time) {
             this.w = w;
             this.time = time;
         }
     }
 
     static int A, B, N;
-    static HashMap<Integer, Pair>[] graph;
-    static long INF = (long) 1e11;
+    static List<Pair>[] graph;
+    static long INF = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        graph = new HashMap[1001];
-        for (int i = 0; i < 1001; i++) {
-            graph[i] = new HashMap<>();
-        }
-
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= N; i++) {
+        graph = new ArrayList[1001];
+        for (int i = 0; i < 1001; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             long price = Long.parseLong(st.nextToken());
             int stopCnt = Integer.parseInt(st.nextToken());
-            int[] route = new int[stopCnt];
 
+            int[] route = new int[stopCnt];
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < stopCnt; j++) {
                 route[j] = Integer.parseInt(st.nextToken());
             }
 
             for (int j = 0; j < stopCnt - 1; j++) {
-                long cumulativeTime = 0;
                 for (int k = j + 1; k < stopCnt; k++) {
-                    cumulativeTime += 1;
-                    updateGraph(route[j], route[k], price, cumulativeTime, i);
+                    int time = k - j;
+                    graph[route[j]].add(new Pair(route[k], price, time));
                 }
             }
         }
 
-        long[] answer = dijk(A, B);
-        System.out.print(answer[0] + " " + answer[1]);
+        long[] answer = dijkstra(A, B);
+        System.out.println(answer[0] + " " + answer[1]);
     }
 
-    public static void updateGraph(int start, int to, long price, long time, int bus) {
-        if (!graph[start].containsKey(to) || graph[start].get(to).w > price || (graph[start].get(to).w == price && graph[start].get(to).time > time)) {
-            graph[start].put(to, new Pair(to, price, bus, (int) time));
-        }
-    }
-
-    public static long[] dijk(int A, int B) {
-        long[] answer = new long[]{-1, -1};
+    public static long[] dijkstra(int start, int end) {
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-            int cmp = Long.compare(o1.w, o2.w);
-            if (cmp == 0) {
+            if (o1.w == o2.w) {
                 return Long.compare(o1.time, o2.time);
             }
-            return cmp;
+            return Long.compare(o1.w, o2.w);
         });
 
-        Cost[] dist = new Cost[1001];
+        long[] minCost = new long[1001];
+        long[] minTime = new long[1001];
+        Arrays.fill(minCost, INF);
+        Arrays.fill(minTime, INF);
 
-        for (int i = 0; i < dist.length; i++) {
-            dist[i] = new Cost(INF, INF);
-        }
-        dist[A] = new Cost(0, 0);
-        pq.add(new Node(A, 0, 0, 0));
+        minCost[start] = 0;
+        minTime[start] = 0;
+        pq.add(new Node(start, 0, 0));
 
         while (!pq.isEmpty()) {
             Node curr = pq.poll();
-            int now = curr.to;
 
-
-
-            if (now == B) {
-                answer[0] = curr.w;
-                answer[1] = curr.time;
-                break;
+            if (curr.to == end) {
+                return new long[]{curr.w, curr.time};
             }
 
-            for (Map.Entry<Integer, Pair> entry : graph[now].entrySet()) {
-                int next = entry.getKey();
-                Pair temp = entry.getValue();
+            for (Pair next : graph[curr.to]) {
+                long newCost = curr.w + next.w;
+                long newTime = curr.time + next.time;
 
-                long newW = curr.w + temp.w;
-                long newTime = curr.time + temp.time;
-
-                if (compare(dist[next], new Cost(newW, newTime))) {
-                    dist[next] = new Cost(newW, newTime);
-                    pq.add(new Node(next, newW, temp.bus, newTime));
+                if (newCost < minCost[next.to] || (newCost == minCost[next.to] && newTime < minTime[next.to])) {
+                    minCost[next.to] = newCost;
+                    minTime[next.to] = newTime;
+                    pq.add(new Node(next.to, newCost, newTime));
                 }
             }
         }
 
-        return answer;
+        return new long[]{-1, -1};
     }
-
-    public static boolean compare(Cost newCost, Cost oldCost) {
-        return newCost.w < oldCost.w || (newCost.w == oldCost.w && newCost.time < oldCost.time);
-    }
-
 }
