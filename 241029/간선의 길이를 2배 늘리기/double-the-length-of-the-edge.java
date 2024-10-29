f472c6ffb57f1af43853e0ec1c9ffad21a475dd3
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-10-29 16:35:54 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 402ms, Memory: 17MB, Status: Passed - CodeTree

--- 241029/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
+++ 241029/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
@@ -7,22 +7,10 @@ public class Main {
     static class Edge{
         int to;
         int w;
-        ArrayList<Integer> path;
-
-        public Edge(int to, int w, ArrayList path){
+        public Edge(int to, int w){
             this.to = to;
             this.w = w;
-            this.path = path;
-        }
-    }
 
-        static class Edge2{
-        int to;
-        int w;
-
-        public Edge2(int to, int w){
-            this.to = to;
-            this.w = w;
         }
     }
 
@@ -30,6 +18,7 @@ public class Main {
     static ArrayList<HashMap<Integer,Integer>> graph;
     static int INF = Integer.MAX_VALUE;
     static int maxD = -1;
+    static int[] dist, before;
 
 
     public static void main(String[] args) throws IOException {
@@ -40,6 +29,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         graph = new ArrayList<>();
+        dist = new int[N+1];
+        before = new int[N+1];
         for(int i=0; i<=N; i++){
             graph.add(new HashMap<>());
         }
@@ -54,18 +45,27 @@ public class Main {
         }
 
 
-        Edge result = dijk(1);
-        int firstD = result.w;
+        dijk(1);
 
-        for(int i=1; i<result.path.size(); i++){
-            int start = result.path.get(i-1);
-            int end = result.path.get(i);
+        int firstD =dist[N];
+        ArrayList<Integer> path = new ArrayList<>();
+        
+        int currentNode = N;
+        while(currentNode !=1){
+            path.add(currentNode);
+            currentNode = before[currentNode];
+        }
+        path.add(1);
+
+        for(int i=1; i<path.size(); i++){
+            int start = path.get(i-1);
+            int end = path.get(i);
             
             int temp = graph.get(start).get(end);
             graph.get(start).put(end,temp*2);
             graph.get(end).put(start,temp*2);
-            
-            maxD = Math.max(maxD, dijk2(1));
+            dijk(1);
+            maxD = Math.max(maxD,dist[N]);
 
             graph.get(start).put(end,temp);
             graph.get(end).put(start,temp);
@@ -76,58 +76,22 @@ public class Main {
 
 
 
-    public static Edge dijk(int start){
-        int[] dist = new int[N+1];
+    public static void dijk(int start){
         Arrays.fill(dist,INF);
+        Arrays.fill(before,-1);
         dist[start] = 0;
+        before[start] = start;
         PriorityQueue<Edge> pq = new PriorityQueue<>((o1,o2)-> o1.w-o2.w);
-        ArrayList<Integer> init = new ArrayList<>();
-        init.add(start);
-        pq.add(new Edge(start,0,init));
+
+        pq.add(new Edge(start,0));
 
         while(!pq.isEmpty()){
             Edge curr = pq.poll();
             int now = curr.to;
             int w = curr.w;
-            ArrayList<Integer> path = curr.path;
 
             if(now == N){
-                return curr;
-            }
-
-
-            if(dist[now]>w) continue;
-
-            for(int next: graph.get(now).keySet()){
-                int newW = w+ graph.get(now).get(next);
-                
-                if(dist[next]>newW){
-                    ArrayList<Integer> newPath = new ArrayList<>(path);
-                    newPath.add(next);
-                    dist[next] = newW;
-                    pq.add(new Edge(next,newW,newPath));
-                }
-            }
-        }
-
-
-        return new Edge(-1,-1,new ArrayList<>());
-    }
-
-        public static int dijk2(int start){
-        int[] dist = new int[N+1];
-        Arrays.fill(dist,INF);
-        dist[start] = 0;
-        PriorityQueue<Edge2> pq = new PriorityQueue<>((o1,o2)-> o1.w-o2.w);
-        pq.add(new Edge2(start,0));
-
-        while(!pq.isEmpty()){
-            Edge2 curr = pq.poll();
-            int now = curr.to;
-            int w = curr.w;
-
-            if(now == N){
-                return w;
+                return;
             }
 
 
@@ -138,12 +102,10 @@ public class Main {
                 
                 if(dist[next]>newW){
                     dist[next] = newW;
-                    pq.add(new Edge2(next,newW));
+                    before[next] = now;
+                    pq.add(new Edge(next,newW));
                 }
             }
         }
-
-
-        return -1;
     }
 }
