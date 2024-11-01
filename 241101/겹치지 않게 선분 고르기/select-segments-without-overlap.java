b383e2e6bb06c4ec44e0a65e90ed43b0d7d218ea
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-11-01 23:02:15 +0900

    Title: 겹치지 않게 선분 고르기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 241101/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 241101/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,61 +1,59 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
     static int n;
-    static int[] visited = new int[1001];
+    static boolean[] visited = new boolean[1001];
     static int[][] lines;
     static int maxCnt = 0;
+
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
+        // 입력 처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringToken st = new StringToken(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         lines = new int[n][2];
-        for(int i=0; i<n; i++){
-            st = new StringToken(br.readLine());
-            lines[i][0]=Integer.parseInt(st.nextToken());
-            lines[i][1]=Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            lines[i][0] = Integer.parseInt(st.nextToken());
+            lines[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        choose(0,0);
+        choose(0, 0);
 
         System.out.print(maxCnt);
-
-
     }
-
-
-
 
     public static void choose(int idx, int cnt){
         if(idx == n){
-            maxCnt = Math.max(maxCnt,cnt);
+            maxCnt = Math.max(maxCnt, cnt);
             return;
         }
 
         ArrayList<Integer> temp = new ArrayList<>();
-        int[] now  = lines[idx];
-        boolean flag =true;
-        
-        int count = 0;
-        for(int i=now[0]; i<=now[1]; i++){
+        int[] now = lines[idx];
+        boolean flag = true;
+
+        for(int i = now[0]; i <= now[1]; i++){
             if(visited[i]) {
-                flag= false;
+                flag = false;
                 break;
             }
             temp.add(i);
-            count++;
             visited[i] = true;
         }
+
         if(flag){
-            choose(idx+1,cnt+count);
+            choose(idx + 1, cnt +1);
         }
+
+        // 방문 해제 (백트래킹)
         for(int back : temp){
-            visited[i] = false;
+            visited[back] = false;
         }
-        choose(idx+1,cnt);
+
+        // 구간을 선택하지 않는 경우도 탐색
+        choose(idx + 1, cnt);
     }
 }
