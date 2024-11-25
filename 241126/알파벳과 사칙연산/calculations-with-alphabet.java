d2ea6a23d4dec4cb884a3b572b36419ab3292923
zziru95 <139435291+zziru95@users.noreply.github.com>
2024-11-26 01:27:29 +0900

    Title: 알파벳과 사칙연산, Time: 92ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 241126/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 241126/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -4,46 +4,78 @@ import java.util.*;
 public class Main {
     static int maxV = Integer.MIN_VALUE;
     static char[] orders;
-    static int n;
+    static char[] alphas;
+    static ArrayList<Integer> numnum = new ArrayList<>();
+    static int n,m;
+    static int[] nums;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String temp = br.readLine();
         orders = new char[temp.length()/2];
+        alphas = new char[orders.length+1];
         int idx = 0;
+        int idx2 = 0;
+        Set<Character> set = new LinkedHashSet<>();
         for(int i=0; i<temp.length(); i++){
             char now = temp.charAt(i);
             if(now =='+' || now == '-' || now =='*'){
                 orders[idx] = now;
                 idx++;
+            } else{
+                set.add(now);
+                alphas[idx2] = now;
+                idx2++;
             }
         }
         n = orders.length;
-            
-        findMax(0,4);
+        nums = new int[6];
+        for(char c: set){
+            numnum.add((int) c -'a');
+        }
+        m = numnum.size();
+        comb(0);
 
         System.out.print(maxV);
     }
 
-    public static void findMax(int idx, int result){
-        if(idx == n){
-            maxV = Math.max(maxV,result);
+
+    public static void comb(int idx){
+        if(idx == m){
+            cal();
             return;
         }
 
-    
-        if(orders[idx] == '+'){
-            findMax(idx+1, 4+result);
-        } else if(orders[idx]== '-'){
-            findMax(idx+1, result-1);
-        } else if(orders[idx]=='*'){
-            if(result>0){
-                findMax(idx+1, result*4);
-            } else{
-                findMax(idx+1, result*1);
+        nums[numnum.get(idx)]= 1;
+        comb(idx+1);
+        nums[numnum.get(idx)]= 4;
+        comb(idx+1);
+
+    }
+
+
+
+    public static void cal(){
+        int idx = 0;
+        int start = nums[(int) alphas[numnum.get(0)] -'a'];
+        int idx2 = 1;
+        char alpha = alphas[idx2];
+        while(idx<n){
+            if(orders[idx] == '+'){
+                start += nums[(int) alpha -'a'];
+            } else if(orders[idx]== '-'){
+                start -= nums[(int) alpha -'a'];
+            } else if(orders[idx]=='*'){
+                start *= nums[(int) alpha -'a'];
             }
-            
+            idx++;
+            idx2++;
         }
-        
+
+        maxV = Math.max(maxV,start);
+        if(maxV ==2){
+            return;
+        }
+
     }
 }
