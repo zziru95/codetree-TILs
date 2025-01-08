84dd0a4fbd630aeecb1bc4d8c4de880628b5a90b
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-01-08 18:48:36 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 175ms, Memory: 19MB, Status: Passed - CodeTree

--- 250108/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 250108/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Main {
     static int K,N;
     static int[] comb;
+    static StringBuilder sb= new StringBuilder();
     public static void main(String[] args) throws IOException {
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,32 +14,25 @@ public class Main {
         comb = new int[N];
 
         choose(0);
+        System.out.print(sb);
     }
 
 
     public static void choose(int idx){
         if(idx==N){
-            StringBuilder sb = new StringBuilder();
-            int cnt = 0;
-            int prev = -1;
             for(int a : comb){
-                if(prev ==a){
-                    cnt++;
-                    if(cnt>=2) return;
-                } else{
-                    cnt =0;
-                    prev = a;
-                }
+
                 sb.append(a).append(" ");
             }
             sb.deleteCharAt(sb.length()-1);
-            System.out.println(sb);
+            sb.append('\n');
 
             return;
         }
 
         for(int i=1; i<=K; i++){
             comb[idx] = i;
+            if(idx>=2 && comb[idx] == comb[idx-1] && comb[idx-2] == comb[idx-1]) continue;
             choose(idx+1);
         }
     }
