ca66fa76e3926fc12739ddef2426c1021006b7b2
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-03 15:15:23 +0900

    Title: 최소 점프 횟수, Time: 79ms, Memory: 9MB, Status: Passed - CodeTree

--- 250203/최소 점프 횟수/min-num-of-jumps.java
+++ 250203/최소 점프 횟수/min-num-of-jumps.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     static int n;
-    static int INF = Integer.MAX_VALUE;
+    
     static int[] jump, memo;
     public static void main(String[] args) throws IOException {
         // Please write your code here.
@@ -14,28 +14,36 @@ public class Main {
         
         jump = new int[n];
         memo = new int[n];
-        Arrays.fill(memo,INF);
+        Arrays.fill(memo,-1);
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             jump[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0,0);
-        System.out.print((memo[n-1] != INF) ? memo[n-1] : -1);
+        bfs();
+        System.out.print(memo[n-1]);
 
     }
 
 
-    public static void dfs(int idx, int t){
-        if(idx>=n){
-            return;
-        }
+    public static void bfs(){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {0,0});
 
-        if(memo[idx] < t) return;
-        memo[idx] = t;
-        int possible = jump[idx];
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int idx = curr[0];
+            int t = curr[1];
 
-        for(int i=1; i+idx<n && i<=possible; i++){
-            dfs(idx+i,t+1);
+            
+            int possible = jump[idx];
+
+            for(int i=1; i+idx<n && i<=possible; i++){
+                if(memo[idx+i]==-1){
+                    memo[idx+i] = t+1;
+                    q.add(new int[] {idx+i,t+1});
+                }        
+            }
+        
         }
     }
 }
