10244406d0e9999027e48004560a9a43b512d2c7
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-03 15:30:57 +0900

    Title: n개 중에 m개 뽑기, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250203/n개 중에 m개 뽑기/n-choose-m.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N,M;
+    static int[] comb;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        comb = new int[M];
+
+        choose(1,0);
+        System.out.print(sb);
+
+    }
+
+
+    public static void choose(int start, int idx){
+        if(idx == M){
+            for(int a : comb){
+                sb.append(a).append(" ");
+            }
+            sb.deleteCharAt(sb.length()-1);
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=start; i<=N; i++){
+            
+            comb[idx] = i;
+            choose(i+1,idx+1);
+        }
+    }
+}
