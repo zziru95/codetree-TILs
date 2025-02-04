81fcd161077b6c5082978812deff266a43f530cd
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-04 13:59:06 +0900

    Title: 피보나치 수, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 250204/피보나치 수/fibonacci-number.java
+++ 250204/피보나치 수/fibonacci-number.java
@@ -4,10 +4,10 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] memo = new int[46];
-        memo[1]=  1;
-        memo[2] = 1;
-        for(int i=3; i<=N; i++){
+        int[] memo = new int[N+1];
+        memo[0]=  0;
+        memo[1] = 1;
+        for(int i=2; i<=N; i++){
             memo[i] = memo[i-1] + memo[i-2];
         }
 
