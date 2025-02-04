0187b38df7724990d526571a79cf18edf3974573
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-04 17:49:20 +0900

    Title: 최대 동전 거슬러주기, Time: 92ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250204/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N,M;
+    static int[] dp,kind;
+
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        kind = new int[N];
+        dp = new int[M+1];
+        for(int i=0; i<N; i++){
+            kind[i] = Integer.parseInt(st.nextToken());
+        }
+        // reverse(kind);
+        for(int a: kind){
+            dp[a] = 1;
+        }
+
+        for(int i=1; i<=M;i++){
+            for(int a: kind){
+                int cnt = 0;
+                if(i-a>=0){
+                    cnt = dp[i-a]+1;
+                }
+                dp[i] = Math.max(dp[i], cnt);
+            }
+        }
+
+        System.out.print(dp[M] != 0 ? dp[M] : -1);
+        
+    }
+
+    public static void reverse(int[] lst){
+        int left = 0;
+        int right = lst.length-1;
+        while(left<right){
+            int temp = lst[left];
+            lst[left] = lst[right];
+            lst[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
