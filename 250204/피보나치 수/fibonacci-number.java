5cfd1bac98446993e846ff8ffcedc96d7956cfc5
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-04 13:54:35 +0900

    Title: 피보나치 수, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250204/피보나치 수/fibonacci-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] memo = new int[N+1];
+        memo[1]=  1;
+        memo[2] = 1;
+        for(int i=3; i<=N; i++){
+            memo[i] = memo[i-1] +memo[i-2];
+        }
+
+        System.out.print(memo[N]);
+    }
+}
