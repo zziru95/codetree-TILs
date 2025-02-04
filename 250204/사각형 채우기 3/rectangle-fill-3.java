e5163405870e94cf751cf0e6a867a73ae726f117
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-04 15:04:52 +0900

    Title: 사각형 채우기 3, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 250204/사각형 채우기 3/rectangle-fill-3.java
+++ 250204/사각형 채우기 3/rectangle-fill-3.java
@@ -1,19 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MOD = 1000000007;  // 오버플로우 방지를 위한 MOD
+    public static final int MAX_N = 1000;  // 최대 n 값
+    
+    // 변수 선언
+    public static int n;
+    public static long[] dp = new long[MAX_N + 1];
+
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[1001];
+        n = sc.nextInt();
+        sc.close();
+
+        // 초기 조건 설정
         dp[0] = 1;
         dp[1] = 2;
-        dp[2] = 7;
-        for(int i=3; i<=n ; i++){
-            dp[i] = (3*dp[i-2] + dp[i-1] *2) % 1000000007;
-        }
 
-        System.out.print(dp[n]);
+        // 점화식 적용
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+            for (int j = i - 3; j >= 0; j--) {
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
+            }
+        }
         
+        System.out.print(dp[n]);
     }
 }
