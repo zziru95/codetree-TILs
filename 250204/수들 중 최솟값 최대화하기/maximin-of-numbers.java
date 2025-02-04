792b4fd0132b0351a7c95023ba38ab408cc76786
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-04 13:49:30 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250204/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int n;
+    static int[][] arr;
+    static ArrayList<Integer> comb;
+    static boolean[] visited;
+    static int answer = Integer.MIN_VALUE;
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n][n];
+        comb = new ArrayList<>();
+        visited = new boolean[n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        comb(0,Integer.MAX_VALUE);
+        System.out.print(answer);
+
+
+
+    }
+
+
+    public static void comb(int idx, int min){
+        if (comb.size() == n){
+            answer = Math.max(answer,min);
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                int temp = arr[i][idx];
+                comb.add(i);
+                comb(idx+1, Math.min(min,temp));
+                visited[i] = false;
+                comb.remove(comb.size()-1);
+            }
+        }
+    }
+
+}
