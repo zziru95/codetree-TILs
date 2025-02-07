fc57877759a0b7607b722e7e93041234e4d3610e
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-07 16:21:37 +0900

    Title: 계단 오르기 2, Time: 100ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250207/계단 오르기 2/climbing-stairs-2.java
+++ 250207/계단 오르기 2/climbing-stairs-2.java
@@ -22,7 +22,7 @@ public class Main {
         
         //2계단 올라간것부터 쌓아가기 시작(1계단 올라간건 초기값으로 넣었으니까)
         for(int i=2; i<=N; i++){
-            //1계단씩 몇번 올랐는지 0번,1번,2번
+            //1계단씩 몇번 올랐는지 0번,1번,2번,3번
             for(int j=0; j<4; j++){
                 dp[i][j] = Math.max(dp[i-2][j] + arr[i], dp[i][j]);
                 
@@ -33,7 +33,8 @@ public class Main {
         }
         int answer = Math.max(dp[N][0],dp[N][1]);
         answer = Math.max(answer,dp[N][2]);
-        System.out.print(answer);
+        
+        System.out.print(dp[N][3]);
                                        
     }
 }
