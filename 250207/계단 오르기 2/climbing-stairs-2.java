c6604833ced0197df9c95e10086f87911ba2ce1d
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-07 16:20:40 +0900

    Title: 계단 오르기 2, Time: 91ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 250207/계단 오르기 2/climbing-stairs-2.java
+++ 250207/계단 오르기 2/climbing-stairs-2.java
@@ -11,18 +11,22 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         arr = new int[N+1];
+        //dp[현재층수][1번오른 횟수]
         dp = new int[N+1][3];
         st = new StringTokenizer(br.readLine());
         for(int i=1; i<=N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        //1층오른 초기값만 지정 1층올랐으니 [1층][1층오르기 1번]->[1][1]
         dp[1][1] = arr[1];
         
+        //2계단 올라간것부터 쌓아가기 시작(1계단 올라간건 초기값으로 넣었으니까)
         for(int i=2; i<=N; i++){
-            for(int j=0; j<3; j++){
-                dp[i][j] = Math.max(dp[i-2][j] + arr[i],dp[i][j]);
+            //1계단씩 몇번 올랐는지 0번,1번,2번
+            for(int j=0; j<4; j++){
+                dp[i][j] = Math.max(dp[i-2][j] + arr[i], dp[i][j]);
                 
-                if(j!=2){
+                if(j!=3){
                     dp[i][j+1] = Math.max(dp[i-1][j] + arr[i],dp[i][j+1]);
                 }
             }
