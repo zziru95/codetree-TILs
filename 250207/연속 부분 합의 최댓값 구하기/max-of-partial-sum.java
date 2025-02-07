a9686b90b40904ca7d6ea13c5760d2cbd7d5b08f
zziru95 <139435291+zziru95@users.noreply.github.com>
2025-02-07 15:19:29 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250207/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int n;
+    static int[] arr, dp;
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dp = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n;i ++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        } 
+        int answer = Integer.MIN_VALUE;
+        dp[0] = arr[0];
+        for(int i=1; i<n; i++){
+            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+            answer = Math.max(dp[i], answer);
+        }
+        System.out.print(answer);
+    }
+}
